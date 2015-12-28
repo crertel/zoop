6f19a31a3e32209455fc92c812d14d3dcc0783d9
Patrick Baggett <baggett.patrick@gmail.com>
2015-12-27 20:21:10 -0600

    Fix some silly leftovers and initialize some more fields for physics.

--- src/server/player.js
+++ src/server/player.js
@@ -9,10 +9,13 @@
 function Player(socket) {
 	this.x = 0;
 	this.y = 0;
+	this.vx = 0;
+	this.vy = 0;
+	this.radius = 0.25;
+	this.facing = 0;
 	this.lastFire = 0;
 	this.socket = socket;
 	this.name = "Unnamed Player";
-	this.state = Player.STATE_CONNECTED;
 }
 
 module.exports = {
