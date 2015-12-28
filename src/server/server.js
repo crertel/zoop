73a8399482e87492b76e25492fcaf86538c26dc9
Patrick Baggett <baggett.patrick@gmail.com>
2015-12-27 19:50:45 -0600

    Basic refactoring of server to have network / world frames

--- src/server/server.js
+++ src/server/server.js
@@ -7,7 +7,10 @@ function URFP(x){
 var http = require('http');
 var ws = require('ws').Server;
 var express = require('express');
+var Player = require('./player.js').Player;
 var kListenPort = process.env.LISTEN_PORT || 3000;
+var kNetworkFrameDelay = 1000 / 20;
+var kWorldFrameDelay = 1000 / 60;
 
 var app = express();
 app.use( express.static(__dirname + '/static') );
@@ -19,24 +22,72 @@ app.get('/', function _index( req,res ) {
 var server = http.createServer();
 var wsServer = new ws({ server: server });
 
-var sockets = [];
+var players = [];
 
-wsServer.on('connection', function _handleConnection( socket ) {
-	console.log('Connection accepted.');
-	socket.on('message', function _dispatchMessage( msg ){
-		console.log('Got message from ', ws, msg);
-		sockets
-		//.filter( function _notMe( sock ) { return sock != socket; } )
-		.forEach( function _sendMsg( socket ){
-			socket.send(msg);
-		});
-	});
-	sockets.push(socket);	
-});
+wsServer.on('connection', onConnection);
 
 server.on('request', app);
+
+//Begin listen
 server.listen(kListenPort, function _serverUp(){
-	console.log('Server up.');
+	console.log('Server listening on port ', kListenPort);
 });
 
- 
+
+function worldFrame() {
+	console.log("World Frame");
+	setTimeout(worldFrame, kWorldFrameDelay);
+}
+
+function networkFrame() {
+	console.log("Network Frame");
+	setTimeout(networkFrame, kNetworkFrameDelay);
+}
+
+/**
+ * Called when a new connection to the server is made. Creates
+ * a new player and adds to the set of players. The existing players
+ * are informed of the new player.
+ * @param socket The socket of the new connector
+ */
+function onConnection(socket) {
+	console.log('New connection from ', socket.address, ' accepted.');
+	
+	var player = new Player(socket);
+	
+	//Inform other players.
+	players.forEach( function informOfNewPlayer(player) {
+		//TODO: something cool here.
+	})
+	
+	socket.on('message', onMessage);
+	socket.on('close', onClose);
+	socket.player = player;
+	players.push(new Player(socket));	
+
+}
+
+/**
+ * Called when a message is received on a socket.
+ */
+function onMessage(message) {
+	var player = this.player;
+	console.log('Got message from ', player.name);
+	/*	
+	players.forEach( function _sendMsg( socket ){
+		socket.send(msg);
+	});*/
+}
+
+/**
+ * Called when a socket is closed.
+ * @param code The reason code, as defined by WebSockets spec.
+ * @param msg The reason as a string.
+ */
+function onClose(code, msg) {
+	var player = this.player;
+	console.log('Player ',player.name,' disconnected, code ', code, '(',msg,')');
+}
+
+setTimeout(worldFrame, kWorldFrameDelay);
+setTimeout(networkFrame, kNetworkFrameDelay);
